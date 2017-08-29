9d6d183761e1842ad009e3f88dca1f11ca03b07c
marcinRG <marcin.guszpit@gmail.com>
2017-08-29 18:06:39 +0200

    changes

--- tests/app/dataService.spec.js
+++ tests/app/dataService.spec.js
@@ -1,5 +1,5 @@
 'use strict';
-var dataService = require('../../src/js/app/dataService/dataService');
+var dataService = require('../../src/js/app/data/dataService');
 
 describe('Testy modułu dataService.js', function () {
     it('', function () {
